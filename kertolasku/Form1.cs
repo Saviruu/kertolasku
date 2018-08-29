76e02fcd708f8e0f94acd3cd4253cc9f65f66346
torppjus <torppjus@gradia.fi>
2018-08-29 16:05:17 +0300

    Add project files.

---
+++ kertolasku/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kertolasku
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i,syote;
+            int[] luvut = new int[3];
+
+
+                try {
+
+                    for (i = 0; i < 3; i++)
+
+                        syote = Convert.ToInt32(textBox1.Text);
+                    for (i = 1; i < 11; i++) { }
+                    tulosL.ForeColor = System.Drawing.Color.Black;
+                    tulosL.Text = i + "*" + syote + " = " + i * syote;
+                    tulosL.Update();
+                }
+                
+
+                catch (System.FormatException)
+                {
+                    tulosL.ForeColor = System.Drawing.Color.Red;
+                    tulosL.Text = "Ohjelmassa tapahtui virhe!";
+                }
+        }
+    }
+}
